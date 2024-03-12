b4f5998af4e7b29418c4591fd1422b247af67b72
JackeyHuang1 <145681430+JackeyHuang1@users.noreply.github.com>
2024-03-12 08:55:02 -0400

    Add files via upload
    Made classes

---
+++ Unit6-9Project/src/Piece.java
@@ -0,0 +1,10 @@
+public class Piece {
+    private int[] position;
+    public Piece(int[] position) {
+        this.position = position;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+}
---
+++ Unit6-9Project/src/Rook.java
@@ -0,0 +1,5 @@
+public class Rook extends Piece{
+    public Rook(int[] position) {
+        super(position);
+    }
+}
