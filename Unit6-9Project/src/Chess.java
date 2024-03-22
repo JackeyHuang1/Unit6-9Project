e9725ec2ce7b9a6ab319a0c8dcf69bb986b2a07e
BT_1N3_18 <jackeyh2@nycstudents.net>
2024-03-22 09:29:36 -0400

    Started checking if valid move

--- Unit6-9Project/src/Chess.java
+++ Unit6-9Project/src/Chess.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Chess {
     private Piece[][] board;
     private Scanner scan;
     private String color;
     private boolean run;
+    private ArrayList pointsWhite;
+    private ArrayList pointsBlack;
     public Chess() {
         scan = new Scanner(System.in);
         createBoard();
@@ -21,7 +24,7 @@ public class Chess {
         }
         for (int i = 0; i < board.length; i++) {
             int[] pos = new int[2];
-            pos[0] = 1;
+            pos[0] = 6;
             pos[1] = i;
             board[6][i] = new Pawn(pos, "White", "♟ ");
         }
@@ -91,7 +94,6 @@ public class Chess {
         String piece = scan.nextLine();
         char char1 = piece.charAt(0);
         int num = char1 - 'a';
-        System.out.println(num);
         if (board[8-Integer.parseInt(piece.substring(1))][num].getColor().equals(color)) {
             System.out.print("To which square? (Give notation such as e4) ");
             String direction = scan.nextLine();
@@ -101,17 +103,25 @@ public class Chess {
                 System.out.println("You can't capture your own piece");
                 move();
             }
-            int[] pos = new int[2];
-            pos[0] = 8-Integer.parseInt(piece.substring(1));
-            pos[1] = num;
-            board[8-Integer.parseInt(direction.substring(1))][destination] = board[8-Integer.parseInt(piece.substring(1))][num];
-            board[8-Integer.parseInt(piece.substring(1))][num] = new Piece(pos, "none", "_   ");
-
-            if (color.equals("White")) {
-                color = "Black";
+            int[] target = new int[2] ;
+            target[0] = 8-Integer.parseInt(direction.substring(1));
+            target[1] = destination;
+            if (board[8-Integer.parseInt(piece.substring(1))][num].isValidMove(target)) {
+                int[] pos = new int[2];
+                pos[0] = 8-Integer.parseInt(piece.substring(1));
+                pos[1] = num;
+                board[8-Integer.parseInt(direction.substring(1))][destination] = board[8-Integer.parseInt(piece.substring(1))][num];
+                board[8-Integer.parseInt(piece.substring(1))][num] = new Piece(pos, "none", "_   ");
+                if (color.equals("White")) {
+                    color = "Black";
+                } else {
+                    color = "White";
+                }
             } else {
-                color = "White";
+                System.out.println("Invalid move");
+                move();
             }
+
         } else {
             System.out.println("Invalid move");
             move();
--- Unit6-9Project/src/Knight.java
+++ Unit6-9Project/src/Knight.java
@@ -2,4 +2,13 @@ public class Knight extends Piece{
     public Knight(int[] position, String color, String name) {
         super(position, color, name);
     }
+
+    public boolean isValidMove(int[] destination) {
+        System.out.println(getPosition()[0] + "," + getPosition()[1]);
+        if (destination[0] == getPosition()[0] + 1 || destination[0] == getPosition()[0] -1) {
+            if (destination[1] == getPosition()[1] - 2 || destination[0] == getPosition()[1] + 2) {
+                return true;
+            }
+        } return false;
+    }
 }
